dc486fd30a393d34798b166429992dabf8de5cb7
valerijus-miscenko <valerijus.miscenko@hpe.com>
2017-11-02 12:30:19 -0700

    Completed Exercise 5.2

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -5,11 +5,11 @@ trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -27,7 +27,7 @@ trait Stream[+A] {
 
   def toList: List[A] = {
     @annotation.tailrec
-    def loop (s: Stream[A], acc: List[A]): List[A] = s match {
+    def loop(s: Stream[A], acc: List[A]): List[A] = s match {
       case Empty => acc
       case Cons(h, t) => loop(t(), h() :: acc)
     }
@@ -35,12 +35,12 @@ trait Stream[+A] {
     loop(this, List()).reverse
   }
 
-  //this is incorrect (returns reversed list)
-  def take(n: Int): Stream[A] = {
-    def loop (s: Stream[A], n: Int, acc: Stream[A]): Stream[A] = {
+  //this is incorrect (returns reversed stream)
+  def take2(n: Int): Stream[A] = {
+    def loop(s: Stream[A], n: Int, acc: Stream[A]): Stream[A] = {
       if (n == 0) acc
       else s match {
-        case Cons(h, t) => loop(t(), n-1, cons(h(), acc))
+        case Cons(h, t) => loop(t(), n - 1, cons(h(), acc))
         case _ => acc
       }
     }
@@ -48,7 +48,28 @@ trait Stream[+A] {
     loop(this, n, Stream())
   }
 
-  def drop(n: Int): Stream[A] = ???
+  def take3(n: Int): Stream[A] = {
+    def loop (s: Stream[A], n: Int): Stream[A] = {
+      if (n == 0) Stream()
+      else s match {
+        case Cons(h, t) => cons(h(), loop(t(), n - 1))
+        case _ => Stream()
+      }
+    }
+    loop(this, n)
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => t().drop(n - 1)
+    // case Cons(_, t) if n == 1 => t() //not needed
+    case _ => this
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = ???
 
