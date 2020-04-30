02a7ff75ffb08ac629bd3754e146a62c5b68e943
vmiscenko@gmail.com <vmiscenko@gmail.com>
2020-04-29 22:03:22 -0700

    completed Exercise 3.27

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -13,5 +13,16 @@ object Tree {
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
+  // 3.26
+  def max(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => max(l) max max(r)
+  }
+
+  // 3.27
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
 }
