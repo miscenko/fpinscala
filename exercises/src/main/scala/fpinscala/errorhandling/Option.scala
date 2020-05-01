91660390e7b930206673dc37adc90b76c0f38c7c
vmiscenko@gmail.com <vmiscenko@gmail.com>
2020-04-30 21:50:42 -0700

    4.1

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,60 +1,51 @@
 package fpinscala.errorhandling
 
 
-import scala.{Either => _, Option => _, Some => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Either => _, Option => _, Some => _}  // hide std library `Option`, `Some` and `Either`,
+                                                    // since we are writing our own in this chapter
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
-      case None => None
-      case Some(a) => Some(f(a))
-    }
+    case None => None
+    case Some(a) => Some(f(a))
+  }
 
   def getOrElse[B>:A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] =
-    this map f getOrElse None
-  /*
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-  def flatMap[B](f: A => Option[B]): Option[B] = this match {
-    case None => None
-    case Some(a) => f(a)
-  }
-  */
+  def orElse[B>:A](ob: => Option[B]): Option[B] = map(Some(_)) getOrElse ob
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] =
-    map (Some(_)) getOrElse ob
-
-  def filter(f: A => Boolean): Option[A] =
-    flatMap(a => if(f(a)) Some(a) else None)
-  /*
-  def filter(f: A => Boolean): Option[A] = this match {
-    case Some(a) if f(a) => this
-    case _ => None
-  }
-  */
+  def filter(f: A => Boolean): Option[A] = flatMap(a => if(f(a)) Some(a) else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
   def failingFn(i: Int): Int = {
-    val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+    val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`,
+                                              // and sets it equal to the right hand side of the `=`.
     try {
       val x = 42 + 5
       x + y
     }
     catch {
       case e: Exception => 43
-    } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    }
+    // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception`
+    // is a pattern that matches any `Exception`, and it binds this value to the identifier `e`.
+    // The match returns the value 43.
   }
 
   def failingFn2(i: Int): Int = {
     try {
       val x = 42 + 5
-      x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type;
+                                                // here we're annotating it with the type `Int`
     }
     catch {
       case e: Exception => 43
