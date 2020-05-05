f67c05121e4111785d0cf8b7d16cd7f1e4a836f6
vmiscenko@gmail.com <vmiscenko@gmail.com>
2020-05-05 16:39:00 -0700

    5.3

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -37,6 +37,24 @@ trait Stream[+A] {
     loop(this)
   }
 
+  // 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => cons(h(), t().take(n - 1))
+    case _ => empty
+  }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+  // 5.3
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h())  => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -52,49 +70,11 @@ trait Stream[+A] {
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
 
-  //this is incorrect (returns reversed stream)
-  def take2(n: Int): Stream[A] = {
-    def loop(s: Stream[A], n: Int, acc: Stream[A]): Stream[A] = {
-      if (n == 0) acc
-      else s match {
-        case Cons(h, t) => loop(t(), n - 1, cons(h(), acc))
-        case _ => acc
-      }
-    }
-
-    loop(this, n, Stream())
-  }
-
-  def take3(n: Int): Stream[A] = {
-    def loop (s: Stream[A], n: Int): Stream[A] = {
-      if (n == 0) Stream()
-      else s match {
-        case Cons(h, t) => cons(h(), loop(t(), n - 1))
-        case _ => Stream()
-      }
-    }
-    loop(this, n)
-  }
-
-  def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
-    case Cons(h, _) if n == 1 => cons(h(), empty)
-    case _ => empty
-  }
-
-  def drop(n: Int): Stream[A] = this match {
-    case Cons(h, t) if n > 0 => t().drop(n - 1)
-    // case Cons(_, t) if n == 1 => t() //not needed
-    case _ => this
-  }
-
-  def takeWhile2(p: A => Boolean): Stream[A] = this match {
-    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
-    case _ => empty
-  }
-
+  /*
   def takeWhile(p: A => Boolean): Stream[A] =
     foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else empty)
+
+   */
 
   def forAll(p: A => Boolean): Boolean =
     foldRight(true)((a, b) => p(a) && b)
