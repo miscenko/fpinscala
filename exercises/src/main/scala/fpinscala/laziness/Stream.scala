f41c8eccc60e95bdf3c462b8f45bc3c79a653c81
vmiscenko@gmail.com <vmiscenko@gmail.com>
2020-05-15 16:46:17 -0700

    5.16

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -150,7 +150,12 @@ trait Stream[+A] {
     tails exists (_ startsWith s)
 
   // 5.16
-  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = ???
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight(z, Stream(z))((a, p0) => {
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
 }
 
 case object Empty extends Stream[Nothing]
