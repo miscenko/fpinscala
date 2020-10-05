be1f0dbe733db8afe3f57f7e61b7122e1fbed14e
misval <vmiscenko@gmail.com>
2020-10-05 14:21:49 -0700

    up through 7.14 (will skip 7.15, 7.16 and 7.17)

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -8,10 +8,12 @@ import language.implicitConversions
 
 object Par {
   type Par[A] = ExecutorService => Future[A]
-  
+
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)  /* `unit` is represented as a function that
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  /* `unit` is represented as a function that
                                                                       returns a `UnitFuture`, which is a simple
                                                                       implementation of `Future` that just wraps a
                                                                       constant value. It doesn't use the
@@ -20,26 +22,29 @@ object Par {
                                                                       returns the value that we gave it. */
 
   private case class UnitFuture[A](get: A) extends Future[A] {
-    def isDone = true 
+    def isDone = true
+
     def get(timeout: Long, units: TimeUnit): A = get
-    def isCancelled = false 
-    def cancel(evenIfRunning: Boolean): Boolean = false 
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
   // 7.4
-  def asyncF[A,B](f: A => B): A => Par[B] =
+  def asyncF[A, B](f: A => B): A => Par[B] =
     a => lazyUnit(f(a))
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] =  /* `map2` doesn't evaluate the call to `f` in a
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = /* `map2` doesn't evaluate the call to `f` in a
                                                                   separate logical thread, in accord with our design
                                                                   choice of having `fork` be the sole function in the
                                                                   API for controlling parallelism. We can always do
                                                                   `fork(map2(a,b)(f))` if we want the evaluation of `f`
                                                                   to occur in a separate thread. */
     (es: ExecutorService) => {
-      val af = a(es) 
+      val af = a(es)
       val bf = b(es)
       UnitFuture(f(af.get, bf.get)) /* This implementation of `map2` does _not_ respect timeouts, and eagerly waits
                                     for the returned futures. This means that even if you have passed in "forked"
@@ -50,7 +55,7 @@ object Par {
                                     of time spent evaluating `af`, then subtracts that time from the available time
                                     allocated for evaluating `bf`. */
     }
-  
+
   def fork[A](a: => Par[A]): Par[A] = /* This is the simplest and most natural implementation of `fork`, but there are
                                       some problems with it--for one, the outer `Callable` will block waiting for the
                                       "inner" task to complete. Since this blocking occupies a thread in our thread
@@ -58,12 +63,12 @@ object Par {
                                       losing out on some potential parallelism. Essentially, we're using two threads
                                       when one should suffice. This is a symptom of a more serious problem with the
                                       implementation, and we will discuss this later in the chapter. */
-    es => es.submit(new Callable[A] { 
+    es => es.submit(new Callable[A] {
       def call: A = a(es).get
     })
 
-  def map[A,B](pa: Par[A])(f: A => B): Par[B] = 
-    map2(pa, unit(()))((a,_) => f(a))
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] =
+    map2(pa, unit(()))((a, _) => f(a))
 
   def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
 
@@ -71,7 +76,7 @@ object Par {
   def sequence[A](ps: List[Par[A]]): Par[List[A]] =
     ps.foldRight(unit(List[A]()))((a, b) => map2(a, b)(_ :: _))
 
-  def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = fork {
     val fbs: List[Par[B]] = ps.map(asyncF(f))
     sequence(fbs)
   }
@@ -85,28 +90,62 @@ object Par {
   def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean =
     p(e).get == p2(e).get
 
-  def delay[A](fa: => Par[A]): Par[A] = 
+  def delay[A](fa: => Par[A]): Par[A] =
     es => fa(es)
 
   def choice[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
-    es => 
+    es =>
       if (run(es)(cond).get) t(es) // Notice we are blocking on the result of `cond`.
       else f(es)
+
+  // 7.11
+  def choiceN[A](n: Par[Int])(choices: List[Par[A]]): Par[A] =
+    es => run(es)(choices(run(es)(n).get))
+
+  def choiceViaChoiceN[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+    choiceN(map(cond)(b => if (b) 1 else 0))(List(t, f))
+
+  // 7.12
+  def choiceMap[K, V](key: Par[K])(choices: Map[K, Par[V]]): Par[V] =
+    es => run(es)(choices(run(es)(key).get))
+
+  // 7.13
+  def flatMap[A,B](pa: Par[A])(choices: A => Par[B]): Par[B] =
+    es => run(es)(choices(run(es)(pa).get))
+
+  def choiceViaFlatMap[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+    flatMap(cond)(b => if (b) f else t)
+
+  def choiceNViaFlatMap[A](n: Par[Int])(choices: List[Par[A]]): Par[A] =
+    flatMap(n)(i => choices(i))
+
+  // 7.14
+  def join[A](a: Par[Par[A]]): Par[A] =
+    es => run(es)(run(es)(a).get())
+
+  def flatMapViaJoin[A,B](pa: Par[A])(choices: A => Par[B]): Par[B] =
+    join(map(pa)(choices))
+
+  def joinViaFlatMap[A](a: Par[Par[A]]): Par[A] =
+    flatMap(a)(x => x)
 
   /* Gives us infix syntax for `Par`. */
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 
   class ParOps[A](p: Par[A]) {
   }
+
 }
 
 object Examples {
+
   import Par._
+
   def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
     if (ints.size <= 1)
       ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
-    else { 
-      val (l,r) = ints.splitAt(ints.length/2) // Divide the sequence in half using the `splitAt` function.
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2) // Divide the sequence in half using the `splitAt` function.
       sum(l) + sum(r) // Recursively sum both halves and add the results together.
     }
 
